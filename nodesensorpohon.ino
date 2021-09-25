f277ce3197ab0b3ec00678243ee7521a6e729024
teguhsasongko.id <teguhsasongko@users.noreply.github.com>
2021-09-25 21:37:11 +0700

    Update nodesensorpohon.ino

--- nodesensorpohon.ino
+++ nodesensorpohon.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 const char* ssid     = "localhost";
-const char* password = "27BF7F16";
+const char* password = "";
 const char* host = "192.168.1.9";
 const int port = 80;
 
