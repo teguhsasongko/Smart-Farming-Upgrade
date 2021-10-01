1fd505f3eb7b3c42145c0fa97d6016b2f46d65f1
teguhsasongko.id <teguhsasongko@users.noreply.github.com>
2021-10-01 14:26:16 +0700

    Update nodeproses.ino

--- nodeproses.ino
+++ nodeproses.ino
@@ -1,4 +1,6 @@
 #include <Wire.h> //LCD Library
+#include <SoftwareSerial.h>
+SoftwareSerial espSerial(9, 10);
 #include <LiquidCrystal_I2C.h> // LCD library using from  https://www.ardumotive.com/i2clcden.html for the i2c LCD library 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 after finding it from serial monitor (see comment above) for a 16 chars and 2 line display
 
@@ -8,46 +10,76 @@ String humi1;
 String tempe1;
 String pohon2;
 String ph2;
+String air;
+String pupuk;
+String pompaair;
+String pompapupuk;
+
+//RELAY
+int pumpair = 3; //relay pompa air
+int pumppupuk = 4; //relay pompa pupuk
 
 void setup() {
-// Open serial communications and wait for port to open:
-Serial.begin(9600);
-lcd.init(); // initialize the lcd 
-lcd.backlight();
-lcd.setBacklight(HIGH);
-while (!Serial) {
-; // wait for serial port to connect. Needed for native USB port only
-}
-}
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  espSerial.begin(9600);
+  lcd.init(); // initialize the lcd 
+  lcd.backlight();
+  lcd.setBacklight(HIGH);
+  while (!Serial){
+    ; // wait for serial port to connect. Needed for native USB port only
+    }
+  
+  pinMode(pumpair, OUTPUT); // Pompa Kelembaban
+  pinMode(pumppupuk, OUTPUT); // Pompa pH
+  }
+  
 void loop() { // run over and over
 if (Serial.available()) {
-//Serial.write(Serial.read());
-String line = Serial.readString();
-    pohon1 = String(getValue(line,' ',0));
-    Serial.print("Pohon 1: ");
-    Serial.println(pohon1); 
+  //Serial.write(Serial.read());
+  String line = Serial.readString();
+  pohon1 = String(getValue(line,' ',0));
+  Serial.print("Pohon 1: ");
+  Serial.println(pohon1); 
 
-    ph1 = String(getValue(line,' ',1));
-    Serial.print("pH Tanah 1:");
-    Serial.println(ph1); 
+  ph1 = String(getValue(line,' ',1));
+  Serial.print("pH Tanah 1:");
+  Serial.println(ph1); 
 
-    humi1 = String(getValue(line,' ',2));
-    Serial.print("Humidity: ");
-    Serial.println(humi1); 
+  humi1 = String(getValue(line,' ',2));
+  Serial.print("Humidity: ");
+  Serial.println(humi1); 
 
-    tempe1 = String(getValue(line,' ',3));
-    Serial.print("Suhu:");
-    Serial.println(tempe1); 
+  tempe1 = String(getValue(line,' ',3));
+  Serial.print("Suhu:");
+  Serial.println(tempe1); 
     
-    pohon2 = String(getValue(line,' ',4));
-    Serial.print("Pohon 2: ");
-    Serial.println(pohon2); 
+  pohon2 = String(getValue(line,' ',4));
+  Serial.print("Pohon 2: ");
+  Serial.println(pohon2); 
 
-    ph2 = String(getValue(line,' ',5));
-    Serial.print("pH Tanah:");
-    Serial.println(ph2); 
-    }
+  ph2 = String(getValue(line,' ',5));
+  Serial.print("pH Tanah:");
+  Serial.println(ph2); 
+    
+  air = String(getValue(line,' ',6));
+  Serial.print("Ketersediaan Air: ");
+  Serial.println(air); 
 
+  pupuk = String(getValue(line,' ',7));
+  Serial.print("Ketersediaan Pupuk:");
+  Serial.println(pupuk); 
+    
+  pompaair = String(getValue(line,' ',8));
+  Serial.print("Pompa Air: ");
+  Serial.println(pompaair); 
+    
+  pompapupuk = String(getValue(line,' ',9));
+  Serial.print("Pompa Pupuk: ");
+  Serial.println(pompapupuk); 
+  }
+
+///////////////////////////////////////////////////////////////////////////
 //Kelembapan Pohon 1
 if(pohon1 >= String(700)){
   lcd.clear();
@@ -56,6 +88,14 @@ if(pohon1 >= String(700)){
   lcd.print(pohon1);
   lcd.setCursor(0, 1);
   lcd.print("Tanah Kering");
+  delay(1000);
+  digitalWrite(pumpair, LOW);
+  pompaair = "ON";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("ON");
   delay(1000);
 }
 else if(pohon1 >= String(350) && pohon1 <= String(700)){
@@ -66,6 +106,14 @@ else if(pohon1 >= String(350) && pohon1 <= String(700)){
   lcd.setCursor(0, 1);
   lcd.print("Tanah Normal");
   delay(1000);
+  digitalWrite(pumpair, LOW);
+  pompaair = "OFF";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("OFF");
+  delay(1000);
 }
 else if(pohon1 <= String(350)){
   lcd.clear();
@@ -74,6 +122,15 @@ else if(pohon1 <= String(350)){
   lcd.print(pohon1);
   lcd.setCursor(0, 1);
   lcd.print("Tanah Lembab");
+  delay(1000);
+  digitalWrite(pumpair, HIGH);
+  pompaair = "OFF";
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("OFF");
   delay(1000);
 }
   
@@ -86,6 +143,14 @@ if(pohon2 >= String(700)){
   lcd.setCursor(0, 1);
   lcd.print("Tanah Kering");
   delay(1000);
+  digitalWrite(pumpair, LOW);
+  pompaair = "ON";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("ON");
+  delay(1000);
 }
 else if(pohon2 >= String(350) && pohon2 <= String(700)){
   lcd.clear();
@@ -95,6 +160,14 @@ else if(pohon2 >= String(350) && pohon2 <= String(700)){
   lcd.setCursor(0, 1);
   lcd.print("Tanah Normal");
   delay(1000);
+  digitalWrite(pumpair, LOW);
+  pompaair = "OFF";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("OFF");
+  delay(1000);
 }
 else if(pohon2 <= String(350)){
   lcd.clear();
@@ -103,6 +176,14 @@ else if(pohon2 <= String(350)){
   lcd.print(pohon2);
   lcd.setCursor(0, 1);
   lcd.print("Tanah Lembab");
+  delay(1000);
+  digitalWrite(pumpair, HIGH);
+  pompaair = "OFF";
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pompa Air");
+  lcd.setCursor(0, 1);
+  lcd.print("OFF");
   delay(1000);
 }
 
@@ -115,7 +196,7 @@ else if(pohon2 <= String(350)){
   lcd.setCursor(0,1);
   lcd.print("Humidity: ");
   lcd.print(humi1);
-  lcd.println(" %");
+  lcd.print(" %");
   delay(1000);
   
 //PH Tanah dari Pohon 2    
@@ -125,7 +206,15 @@ else if(pohon2 <= String(350)){
     lcd.print("pH = ");
     lcd.print(ph2);
     lcd.setCursor(0,1);
-    lcd.print("SIRAM PUPUK CUK");
+    lcd.print("Tanah Perlu Pupuk");
+    delay(1000);
+    digitalWrite(pumppupuk,HIGH);
+    pompapupuk = "ON";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pompa Pupuk");
+    lcd.setCursor(0, 1);
+    lcd.print("ON");
     delay(1000);
    }
    else if(ph2 > String(6,6)){
@@ -134,9 +223,81 @@ else if(pohon2 <= String(350)){
     lcd.print("pH = ");
     lcd.print(ph2);
     lcd.setCursor(0,1);
-    lcd.print("PUPUK CUKUP CUK");
+    lcd.print("Pupuk Cukup");
+    delay(1000);
+    digitalWrite(pumppupuk,LOW);
+    pompapupuk = "OFF";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pompa Pupuk");
+    lcd.setCursor(0, 1);
+    lcd.print("OFF");
     delay(1000);
    } 
+
+//Ketinggian Air
+ if(air > String(70)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Air = ");
+    lcd.print(air);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Air Masih Penuh");
+    delay(1000);
+ }
+ else if(air > String(50)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Air = ");
+    lcd.print(air);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Tersisa Setengah");
+    delay(1000);
+ }
+ else if(air > String(35)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Air = ");
+    lcd.print(air);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Tersisa Sedikit");
+    delay(1000);
+ }
+//Ketinggian Pupuk
+ if(pupuk > String(70)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Pupuk = ");
+    lcd.print(pupuk);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Masih Penuh");
+    delay(1000);
+ }
+ else if(pupuk > String(50)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Pupuk = ");
+    lcd.print(pupuk);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Tersisa Setengah");
+    delay(1000);
+ }
+ else if(pupuk > String(35)){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Level Pupuk = ");
+    lcd.print(pupuk);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Tersisa Sedikit");
+    delay(1000);
+ }
+espSerial.println(String(pompaair)+" "+String(pompapupuk));
 
 delay(2000);   
 }
